a808ebb1b0ce8ce0b85359e6246d4c0edc5aea8f
DonatellaC <donatella.carta@mailbox.org>
2021-01-12 21:15:52 +0100

    add: joke's api, async/await

--- index.js
+++ index.js
@@ -3,3 +3,31 @@
 const joke = document.getElementById("joke");
 const next_joke = document.getElementById("next-joke");
 
+const joke = document.getElementById("joke");
+const next_joke = document.getElementById("next-joke");
+
+//API
+const apiJoke = "https://icanhazdadjoke.com/";
+
+next_joke.addEventListener("click", generateJoke);
+
+async function generateJoke() {
+    //call the API
+
+    const response = await fetch(apiJoke, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    const data = await response.json();
+
+    const listJokes = data.joke;
+
+    //set the new joke
+    joke.innerHTML = ' " ' + listJokes + ' " ' ;
+    console.log(listJokes);
+}
+
+generateJoke();
+
