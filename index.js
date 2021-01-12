4e98a876c96066ed3efbd77a357f887610548a26
DonatellaC <donatella.carta@mailbox.org>
2021-01-12 21:47:25 +0100

    add: second API joke, functionality for random joke

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 const joke = document.getElementById("joke");
 const next_joke = document.getElementById("next-joke");
 
-//API
+//APIs
 const apiJoke = "https://icanhazdadjoke.com/";
+const chuckNorrisJoke= "https://api.chucknorris.io/jokes/random";
 
 next_joke.addEventListener("click", generateJoke);
 
 async function generateJoke() {
-    //call the API
+    //call the APIs
 
     const response = await fetch(apiJoke, {
         headers: {
@@ -17,13 +18,25 @@ async function generateJoke() {
         }
     });
 
-    const data = await response.json();
+    const responseB = await fetch(chuckNorrisJoke, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
 
-    const listJokes = data.joke;
+    const data = await response.json();
+    const dataB = await responseB.json();
+
+    const listJokes = [ (data.joke), (dataB.value)]
+
+    //set random jokes from APIS
+    const randomJokes = Math.floor(Math.random() * (listJokes.length));
 
     //set the new joke
-    joke.innerHTML = ' " ' + listJokes + ' " ' ;
-    console.log(listJokes);
+    joke.innerHTML = ' " ' + listJokes[randomJokes] + ' " ' ;
+    console.log(randomJokes);
 }
 
 generateJoke();
+
+
